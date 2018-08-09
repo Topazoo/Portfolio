3fdeb0b0e179aba553d8a4655ac643ca847b2625
Ubuntu <ubuntu@ip-172-31-21-157.us-west-2.compute.internal>
2018-08-09 21:48:18 +0000

    Deployed to AWS

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k6mtaccd%_y%jtf2@jm0wpmj-vys3(7h(wvkdh%==kep&0)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.88.119.57', u'52.88.119.57', '*'] #TODO - Remove * and change server sec group
 
 
 # Application definition
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
