bc2d3c017d21252f22b354fc1c32c5c2075c9ca2
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-08-08 18:48:24 -0700

    Fixed JS frontend issue :)

--- portfolio/base/static/js/tracking.js
+++ portfolio/base/static/js/tracking.js
@@ -1,5 +1,5 @@
-var slideIndex = 0;
-showSlides(slideIndex);
+
+var slideIndex = 1;
 
 // Next/previous controls
 function plusSlides(n) {
@@ -12,24 +12,22 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i = n % 2;
+
     var slides = document.getElementsByClassName("slides");
 
-    slides[(n % 2)].style.display = "none";
-    slides[((n + 1) % 2)].style.display = "block";
+    if (n > slides.length)
+        slideIndex = 1;
 
+    if (n < 1) 
+        slideIndex = slides.length;
     
-    //if (n > slides.length)
-      //  slideIndex = 1;
+    for (i = 0; i < slides.length; i++)
+        slides[i].style.display = "none"; 
 
-    //if (n < 1) 
-        //slideIndex = slides.length;
-
-    //for (i = 0; i < slides.length; i++) 
-      //  slides[i].style.display = "none"; 
-
-   // if(slides)
-      //  slides[slideIndex-1].style.display = "block"; 
+    if(slides)
+        slides[slideIndex-1].style.display = "block"; 
 }
 
-
+$(document).ready(function() {
+    showSlides(slideIndex);
+});
