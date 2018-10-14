395b941d3dea3ea73ee4c14ab728cddc60848d3e
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-10-13 19:17:59 -0700

    Full UI revamp

--- portfolio/base/static/js/portfolio-jquery.js
+++ portfolio/base/static/js/portfolio-jquery.js
@@ -73,6 +73,7 @@ $(document).ready(function() {
         } 
         else {
             x.className = "navbk topnav";
+            $(".projdrop-content").hide();
         }
 
         /* Animate icon */
